e96c778b116565280f8a022782c9164f50c916bc
johan <johan.oxenstierna@kairoslogic.com>
2025-07-15 11:20:54 +0200

    MUTAPA

--- src/logger_keys.py
+++ src/logger_keys.py
@@ -6,11 +6,11 @@ logger_keys = {
     2: "Resolution", # 1280 vs 1920
     3: "FPS", # 60, 100, 144
     4: ("Computer", {0: "Laptop", 1: "Workstation"}),
-    5: ("WRITE", {0: "No", 1: "Yes"}),  # prebuild first 100 filenames
-    6: "# PNGs",
+    5: ("WRITE", {0: "No", 1: "Yes"}),  # Whether to show live (No) or write to mp4 (Yes)
+    6: "# PNGs",  # 1 - 20 Planets/moons etc
     7: "PNG Avg. Diameter (px)",
-    8: "# Dots",
-    9: "Perf Time (sec per 60s)",
+    8: "# Dots",  # Rockets
+    9: "Perf Time (sec per 60s)",  # COMPUTATIONAL PERFORMANCE (WE WANT THIS MINIMIZED)
     10: "RAM (MB)",
     11: "Visual Quality",
     12: "Num Frames",
